e00ecc887ec5c4fccefa09b057a97bcd8df019ac
merlinbergmann <hello@merlin.codes>
2018-02-16 11:41:05 +0100

    b2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        String parameter = "This is some text";
+
+        System.out.println(parameter);
     }
+
+
 }
